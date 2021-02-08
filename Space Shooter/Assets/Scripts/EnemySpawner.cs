6dc45668c05e3601781ae6632f675eddbe64283b
Luigi Lantin <mushymanee@gmail.com>
2021-02-07 19:31:26 -0800

    updated enemy movement

--- Space Shooter/Assets/Scripts/EnemySpawner.cs
+++ Space Shooter/Assets/Scripts/EnemySpawner.cs
@@ -5,21 +5,30 @@ using UnityEngine;
 public class EnemySpawner : MonoBehaviour
 {
     [SerializeField] List<WaveConfig> waveConfigs;
-    int startingWave = 0;
+    [SerializeField] int startingWave = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        var currentWave = waveConfigs[startingWave];
-        StartCoroutine(SpawnAllEnemiesInWave(currentWave));
+        StartCoroutine(SpawnAllWaves());
+    }
+
+    private IEnumerator SpawnAllWaves()
+    {
+        for (int waveIndex = startingWave; waveIndex < waveConfigs.Count; waveIndex++)
+        {
+            var currentWave = waveConfigs[waveIndex];
+            yield return StartCoroutine(SpawnAllEnemiesInWave(currentWave));
+        }
     }
 
     private IEnumerator SpawnAllEnemiesInWave(WaveConfig waveConfig)
     {
         for (int i = 0; i < waveConfig.GetNumberOfEnemies(); i++)
         {
-            Instantiate(waveConfig.GetEnemyPrefab(),
+            var newEnemy = Instantiate(waveConfig.GetEnemyPrefab(),
                 waveConfig.GetWaypoints()[0].transform.position, Quaternion.identity);
+            newEnemy.GetComponent<EnemyPathing>().SetWaveConfig(waveConfig);
             yield return new WaitForSeconds(waveConfig.GetTimeBetweenSpawns());
         }
     }
